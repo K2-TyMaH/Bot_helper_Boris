5741ed336a38cf2cf7cc2740ca224d6d034cddde
TyMaH <dma.employee.v@gmail.com>
2022-12-29 14:11:00 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name="Best bot",
-    version="1.0.0",
-    author="Best team",
-    url="https://github.com/LevytskyiS/t_bot",
+    name="Bot-helper Boris",
+    version="2.0.0",
+    author="MRTeam",
+    url="https://github.com/K2-TyMaH/Bot_helper_Boris",
     license="MIT License",
     packages=find_namespace_packages(),
-    entry_points={"console_scripts": ["best_bot = t_bot.main:main"]}
+    entry_points={"console_scripts": ["boris = t_bot.main:main"]}
 )
